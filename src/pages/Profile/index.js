bdee1dc632467dae3a005247ebb1249bb944f6c7
Pedro Gomes <pedrogomes2101@gmail.com>
2022-03-09 10:08:26 -0300

    Group 10 Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 
 export default function Profile() {
+  const [email, setEmail] = useState('');
+  useEffect(() => {
+    const userEmail = JSON.parse(localStorage.getItem('email'));
+    setEmail(userEmail.email);
+  }, []);
+
   return (
-    <Header pageName="Profile" />
+    <>
+      <Header pageName="Profile" />
+      <div>
+        <p data-testid="profile-email">
+          { email }
+        </p>
+        <Link to="/done-recipes">
+          <button type="button" data-testid="profile-done-btn">
+            Done Recipes
+          </button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <button type="button" data-testid="profile-favorite-btn">
+            Favorite Recipes
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+          >
+            Logout
+          </button>
+        </Link>
+      </div>
+    </>
   );
 }
